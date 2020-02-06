8161c9859b36fba1c1624904c0197f037ae50bb0
Eduardo Freitas <edusf8700@gmail.com>
2020-02-06 05:01:20 -0200

    Criado o componente de TechList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import './App.css';
-import download from './assets/download.jpg';
+import TechList from './components/TechList'
 
 export default function App() {
   return (
-    <img width="1000" src={download}/>
+    <TechList/>
   );
 }
--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
 
 class TechList extends Component {
-  render() {
+  state = {
+    newTech: '',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native'
+    ]
+  };
 
+  handleInputChange = e => {
+    this.setState({ newTech : e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({ 
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    });
+  }
+
+  render() {
+    
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+        </ul>
+        <input 
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button>Enviar</button>
+      </form>
+    )  
   }
 }
 
