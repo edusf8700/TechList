6747e726e60682203527dca1f5a9d6915aac1fd3
Eduardo Freitas <edusf8700@gmail.com>
2020-02-11 04:00:19 -0200

    Criado a revoção de itens, Criado o componente de TechIntem e PropTypes e DefaultProps

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import TechItem from './TechItem';
+
 class TechList extends Component {
   state = {
     newTech: '',
@@ -23,12 +25,23 @@ class TechList extends Component {
     });
   }
 
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t != tech ) })
+  }
+
   render() {
     
     return(
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+          {this.state.techs.map(tech => 
+            <TechItem 
+              key={tech} 
+              tech={tech} 
+              onDelete={() => this.handleDelete(tech)}
+            /> 
+          )}
+          <TechItem />  
         </ul>
         <input 
           type="text"
